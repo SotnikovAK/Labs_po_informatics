1b8b355a36c6103a2cb04df720aa742d53ed7127
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2023-02-09 12:41:30 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -5,13 +5,14 @@ import sys
 
 gmsh.initialize()
 gmsh.write('eurofighter_typhoon.msh')
+
 gmsh.merge(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'eurofighter_typhoon.stl'))
 
 a = 800
 b = 500
 ugol = 180
 gmsh.model.mesh.classifySurfaces(ugol * math.pi/180., False, False , 0)
-gmsh.model.geo.synchronize()
+gmsh.model.geo.addVolume([gmsh.model.geo.addSurfaceLoop([gmsh.model.getEntities(2)[i][1] for i in range(len(gmsh.model.getEntities(2)))])])
 
 A = [[a,0],[a,b],[0,b],[0,0]]
 point = []
@@ -20,6 +21,7 @@ for i in range(len(A)):
 
 for i in range(len(A)):
     gmsh.model.geo.addLine(point[i], point[(i+1)%len(A)])
+    
 
 gmsh.model.geo.synchronize()
 gmsh.model.mesh.generate(2)
