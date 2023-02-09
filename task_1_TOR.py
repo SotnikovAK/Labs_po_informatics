a5fc3c842898bc4a0a08bd8b0adc3209a396e3a9
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2023-02-09 12:20:45 +0300

    Update task_1_TOR.py

--- task_1_TOR.py
+++ task_1_TOR.py
@@ -11,6 +11,7 @@ gmsh.model.add("task_1_9K330")
 
 TORS = [gmsh.model.occ.addTorus(coord_center[0], coord_center[1], coord_center[2], tor_radiuses[0], tor_radiuses[i]) for i in [1,2]]
 gmsh.model.occ.cut([(dim, TORS[1])], [(dim, TORS[0])], dim)
+gmsh.option.setNumber("Mesh.MeshSizeFactor", 0.3)
 
 gmsh.model.occ.synchronize()
 gmsh.model.mesh.generate(3)
