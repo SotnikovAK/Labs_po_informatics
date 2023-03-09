be8ae4f033bd681d4d0759b01368bcf18cdc6ef2
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2023-03-09 12:05:35 +0300

    Update LAB2.py

--- LAB2.py
+++ LAB2.py
@@ -6,34 +6,64 @@ import os
 
 
 class CalcMesh:
-
-    def __init__(self, nodes_coords, tetrs_points, velocities):
+    def __init__(self, nodes_coords, tetrs_points):
         self.nodes = np.array([nodes_coords[0::3], nodes_coords[1::3], nodes_coords[2::3]])
-        self.norm_raspred = np.linalg.norm(self.nodes, axis=0)
-        self.velocity = (self.norm_raspred * (velocities[1] - velocities[0]) / np.max(self.norm_raspred) + velocities[0]) * self.nodes / self.norm_raspred
+        self.smth = np.power(self.nodes[0, :], 2) + np.power(self.nodes[1, :], 2)
 
-        self.a = - (0.5 * np.linalg.norm(self.velocity, axis=0) ** 1.5)
+        self.velocity = np.zeros(shape=(3, int(len(nodes_coords) / 3)), dtype=np.double)
+        self.x_0, self.y_0 = 0, 0
+        for i in range(0, len(self.nodes[0])):
+            self.x_0 += self.nodes[0][i]
+            self.y_0 += self.nodes[1][i]
+        self.x_0 /= len(self.nodes[0])
+        self.y_0 /= len(self.nodes[0])
+
+        self.modul = np.power(np.power(self.nodes[0] - self.x_0, 2) + np.power(self.nodes[1] - self.y_0, 2), 1/2)
+        self.alpha = []
+        for i in range(0, len(self.nodes[0])):
+            if self.nodes[0][i] - self.x_0 > 0:
+                self.alpha.append(np.arctan((self.nodes[1][i] - self.y_0) / (self.nodes[0][i] - self.x_0)))
+            else:
+                self.alpha.append(np.pi + np.arctan((self.nodes[1][i] - self.y_0) / (self.nodes[0][i] - self.x_0)))
+
 
         self.tetrs = np.array([tetrs_points[0::4], tetrs_points[1::4], tetrs_points[2::4], tetrs_points[3::4]])
         self.tetrs -= 1
 
-    def move(self, tau):
-        self.velocity += self.a * tau
-        self.nodes += self.velocity * tau
+        self.velocity[0] = -self.modul * np.sin(self.alpha)
+        self.velocity[1] = self.modul * np.cos(self.alpha)
+        self.velocity[2] = 0
+
+    def move(self):
+        self.nodes[0] = self.modul * np.cos(self.alpha) + self.x_0
+        self.nodes[1] = self.modul * np.sin(self.alpha) + self.y_0
+        self.velocity[0] = - self.modul * np.sin(self.alpha)
+        self.velocity[1] = self.modul * np.cos(self.alpha)
+        for i in range(0, len(self.alpha)):
+            self.alpha[i] += 0.1
+
+        self.smth = np.power(self.nodes[0, :], 2) + np.power(self.nodes[1, :], 2)
 
     def snapshot(self, snap_number):
         unstructuredGrid = vtk.vtkUnstructuredGrid()
         points = vtk.vtkPoints()
 
+        smth = vtk.vtkDoubleArray()
+        smth.SetName("smth")
+
         vel = vtk.vtkDoubleArray()
         vel.SetNumberOfComponents(3)
-        vel.SetName("velocity")
+        vel.SetName("vel")
+
 
         for i in range(0, len(self.nodes[0])):
             points.InsertNextPoint(self.nodes[0, i], self.nodes[1, i], self.nodes[2, i])
+            smth.InsertNextValue(self.smth[i])
             vel.InsertNextTuple((self.velocity[0, i], self.velocity[1, i], self.velocity[2, i]))
 
         unstructuredGrid.SetPoints(points)
+
+        unstructuredGrid.GetPointData().AddArray(smth)
         unstructuredGrid.GetPointData().AddArray(vel)
 
         for i in range(0, len(self.tetrs[0])):
@@ -44,11 +74,12 @@ class CalcMesh:
 
         writer = vtk.vtkXMLUnstructuredGridWriter()
         writer.SetInputDataObject(unstructuredGrid)
-        writer.SetFileName("CAT-" + str(snap_number) + ".vtu")
+        writer.SetFileName("cat_step" + str(snap_number) + ".vtu")
         writer.Write()
 
 
 gmsh.initialize()
+
 try:
     path = os.path.dirname(os.path.abspath(__file__))
     gmsh.merge(os.path.join(path, 'geometric cat.stl'))
@@ -57,9 +88,9 @@ except:
     gmsh.finalize()
     exit(-1)
 
-angle = 40
-forceParametrizablePatches = False
-includeBoundary = True
+angle =10
+forceParametrizablePatches = True
+includeBoundary = False
 curveAngle = 180
 gmsh.model.mesh.classifySurfaces(angle * math.pi / 180., includeBoundary, forceParametrizablePatches,
                                  curveAngle * math.pi / 180.)
@@ -72,7 +103,8 @@ gmsh.model.geo.addVolume([l])
 gmsh.model.geo.synchronize()
 
 f = gmsh.model.mesh.field.add("MathEval")
-gmsh.model.mesh.field.setString(f, "F", "4")
+gmsh.model.mesh.field.setString(f, "F", "1")
+gmsh.model.mesh.createGeometry()
 gmsh.model.mesh.field.setAsBackgroundMesh(f)
 
 gmsh.model.mesh.generate(3)
@@ -98,11 +130,10 @@ for i in range(0, len(nodeTags)):
     assert (i == nodeTags[i] - 1)
 assert (len(tetrsNodesTags) % 4 == 0)
 
-temperatures = np.array([1, 500])
-velocities = np.array([6, 2])
-mesh = CalcMesh(nodesCoord, tetrsNodesTags, velocities)
-for i in range(10):
-    mesh.snapshot(i)
-    mesh.move(0.05)
+mesh = CalcMesh(nodesCoord, tetrsNodesTags)
+mesh.snapshot(0)
 
 gmsh.finalize()
+for i in range(1, 100):
+    mesh.move()
+    mesh.snapshot(i)
